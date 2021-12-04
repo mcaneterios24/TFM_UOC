e0d7c04989a5e87a0a898e40e5e4b0c15cea13e5
mcaneterios24 <74266787+mcaneterios24@users.noreply.github.com>
2021-12-04 17:44:16 +0100

    Update 05_decision_trees.R

--- 05_decision_trees.R
+++ 05_decision_trees.R
@@ -1,6 +1,6 @@
 
-### Manuel Cañete Ríos
-### 08/11/2021
+### Manuel CaÃ±ete RÃ­os
+### 04/12/2021
 ### This script will be used to perform DT classification
 
 ### Dependencies
@@ -18,63 +18,92 @@ cl <- makePSOCKcluster(12)
 registerDoParallel(cl)
 
 ### Input directory
-input_dir <- "C:/Users/mcr3927/Desktop/TFM/Inputs"
+input_dir <- "path/to/files"
 
-
-### We keep the pixels of interest
+### We import the input data (only informative pixels and dimensions reduced using PCA)
 arranged_data <- fread(file.path(input_dir, "pca_data_1024.txt")) %>%
   mutate(Pattern = factor(Pattern))
 
-
-### We generate the training and test datasets
+### We generate the training and test datasets (3:1, respectively)
 set.seed(123456)
 indxTrain <- createDataPartition(y = arranged_data$Pattern, p = 0.75, list = FALSE)
 training <- arranged_data[indxTrain,]
 testing <- arranged_data[-indxTrain,]
 
-
-### Generate DT model
+### Declare cross-validation settings
 set.seed(654321)
-ctrl <- trainControl(method = "repeatedcv", 
-                     repeats = 5, 
+ctrl <- trainControl(method = "repeatedcv", # use repeated k-fold cross-validation
+                     repeats = 5, # 5-fold cross-validation
                      classProbs = T, 
                      summaryFunction = twoClassSummary, 
                      returnData = F, 
                      trim = T) 
 
-treeFit <- train(Pattern ~ ., 
+### Generate CART model
+treeFit1 <- train(Pattern ~ ., 
+                 data = training, 
+                 method = "rpart", 
+                 trControl = ctrl,
+                 tuneLength = 20, # test 20 different values of Cp
+                 metric = "ROC")
+
+### Generate C5.0 model
+treeFit2 <- train(Pattern ~ ., 
                 data = training, 
                 method = "C5.0", 
                 trControl = ctrl,
                 tuneLength = 20,
                 metric = "ROC")
 
-treeFit2 <- train(Pattern ~ ., 
-                 data = training, 
-                 method = "rpart", 
-                 trControl = ctrl,
-                 tuneLength = 20,
-                 metric = "ROC")
-
-### We print DT model info
+### We print DT models info
 treeFit
 treeFit2
 
 ### We plot the cross-validation results
+# CART
 plot(treeFit)
+
+cart %>% # This cart object has been created in an Excel file using the output of printing treeFit into R console
+  pivot_longer(-cp, names_to = "Metric", values_to = "Value") %>%
+  ggplot(mapping = aes(x = cp, y = Value, group = Metric, col = Metric)) +
+  geom_point(show.legend = F) +
+  geom_line(show.legend = F) +
+  theme_bw() +
+  theme(text = element_text(size = 12)) +
+  facet_wrap(~Metric, scales = "free") +
+  scale_color_manual(values = c("#003049", "#D62828", "#FCBF49")) +
+  scale_x_continuous(breaks = round(seq(from = 0, to = 0.3, length.out = 8),2))
+
+ggsave("CV_cart.pdf", plot = last_plot(), width = 300, height = 100, units = "mm")
+
+
+#C5.0
 plot(treeFit2)
 
+c5 %>% # This c5 object has been created in an Excel file using the output of printing treeFit into R console
+  pivot_longer(-c("trials", "model", "winnow"), names_to = "Metric", values_to = "Value") %>%
+  ggplot(mapping = aes(x = trials, y = Value, group = Metric, col = Metric)) +
+  geom_point(show.legend = F) +
+  geom_line(show.legend = F) +
+  theme_bw() +
+  theme(text = element_text(size = 12)) +
+  facet_wrap(~Metric+model+winnow, scales = "free") +
+  scale_color_manual(values = c("#003049", "#D62828", "#FCBF49"))
 
-### We use this model to predict classes on the test data
+ggsave("CV_c5.pdf", plot = last_plot(), width = 300, height = 250, units = "mm")
+
+
+### We use these models to predict classes on the test data
+# CART
 pred <- predict(treeFit, testing)
 confusionMatrix(pred, testing$Pattern)
-resROC <- roc(testing$Pattern, as.numeric(pred))
-plot(resROC)
 
+# C5.0
 pred2 <- predict(treeFit2, testing)
 confusionMatrix(pred2, testing$Pattern)
-resROC2 <- roc(testing$Pattern, as.numeric(pred2))
-plot(resROC2)
 
 ### We stop parallel computing
 stopCluster(cl)
+
+### We print the system information
+Sys.info()
