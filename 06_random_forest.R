9e6566f49fe762dd7d9acb6d1705a7b9fe471fc2
mcaneterios24 <74266787+mcaneterios24@users.noreply.github.com>
2021-12-04 17:55:43 +0100

    Update 06_random_forest.R

--- 06_random_forest.R
+++ 06_random_forest.R
@@ -1,6 +1,6 @@
 
-### Manuel Cañete Ríos
-### 08/11/2021
+### Manuel CaÃ±ete RÃ­os
+### 04/12/2021
 ### This script will be used to perform RF classification
 
 ### Dependencies
@@ -18,34 +18,34 @@ cl <- makePSOCKcluster(12)
 registerDoParallel(cl)
 
 ### Input directory
-input_dir <- "C:/Users/mcr3927/Desktop/TFM/Inputs"
+input_dir <- "path/to/files"
 
-
-### We keep the pixels of interest
+### We import the input data (only informative pixels and dimensions reduced using PCA)
 arranged_data <- fread(file.path(input_dir, "pca_data_1024.txt")) %>%
   mutate(Pattern = factor(Pattern))
 
-### We generate the training and test datasets
+### We generate the training and test datasets (3:1, respectively)
 set.seed(123456)
 indxTrain <- createDataPartition(y = arranged_data$Pattern, p = 0.75, list = FALSE)
 training <- arranged_data[indxTrain,]
 testing <- arranged_data[-indxTrain,]
 
-### Generate DT model
+### Declare cross-validation settings
 set.seed(654321)
-ctrl <- trainControl(method = "repeatedcv", 
-                     repeats = 5, 
+ctrl <- trainControl(method = "repeatedcv", # use repeated k-fold cross-validation
+                     repeats = 5,  # 5-fold cross-validation
                      classProbs = T, 
                      summaryFunction = twoClassSummary, 
                      returnData = F, 
                      trim = T) 
 
+### Generate RF model
 RFFit <- train(Pattern ~ ., 
                 data = training, 
                 method = "rf",
-                ntree = 500,
+                ntree = 500, # use 500 trees
                 trControl = ctrl,
-                tuneLength = 15,
+                tuneLength = 15, #Try 15 different values of mtry
                 metric = "ROC")
 
 ### We print RF model info
@@ -54,11 +54,24 @@ RFFit
 ### We plot the cross-validation results
 plot(RFFit)
 
+rf %>% # This rf object has been created in an Excel file using the output of printing RFFit into R console
+  pivot_longer(-mtry, names_to = "Metric", values_to = "Value") %>%
+  ggplot(mapping = aes(x = mtry, y = Value, group = Metric, col = Metric)) +
+  geom_point(show.legend = F) +
+  geom_line(show.legend = F) +
+  theme_bw() +
+  theme(text = element_text(size = 12)) +
+  facet_wrap(~Metric, scales = "free") +
+  scale_color_manual(values = c("#003049", "#D62828", "#FCBF49"))
+
+ggsave("CV_rf.pdf", plot = last_plot(), width = 300, height = 100, units = "mm")
+
 ### We use this model to predict classes on the test data
 pred <- predict(RFFit, testing)
 confusionMatrix(pred, testing$Pattern)
-resROC <- roc(testing$Pattern, as.numeric(pred))
-plot(resROC)
 
 ### We stop parallel computing
 stopCluster(cl)
+
+### We print the system information
+Sys.info()
